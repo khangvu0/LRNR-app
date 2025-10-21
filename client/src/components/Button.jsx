b357b4bfd717685f85dd891c722c8482243fb32a
Khang Vu <49794277+khangvu0@users.noreply.github.com>
2025-10-21 18:55:49 -0400

    Updated props

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -2,6 +2,20 @@ import React from 'react';
 import '../styles/variables.css';
 import '../styles/Button.css';
 
-export default function Button({ text }) {
-    return <button className="btn">{text}</button>;
+export default function Button({
+    children,
+    onClick,
+    type = 'button',
+    disabled = false,
+    className = '',
+}) {
+    return (
+        <button
+            type={type}
+            onClick={onClick}
+            disabled={disabled}
+            className={`btn ${className}`}>
+            {children}
+        </button>
+    );
 }
