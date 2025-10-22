f68f5753d186fdba013447d8ec331f12388bbeaa
Khang Vu <49794277+khangvu0@users.noreply.github.com>
2025-10-22 15:15:41 -0400

    Updated Account page to mimic lrnr app

--- client/src/pages/Account.jsx
+++ client/src/pages/Account.jsx
@@ -1,6 +1,37 @@
 import React from 'react';
 import '../styles/Account.css';
+import fireIcon from '../../public/fire.svg';
+import listIcon from '../../public/list.svg';
+import accountIcon from '../../public/person.svg';
 
 export default function Account() {
-    return <div>Account</div>;
+    return (
+        <main className="account-page">
+            <h1 className="title">Account</h1>
+
+            <div className="account-container">
+                <div className="streak">
+                    <img src={fireIcon} alt="streak icon" />
+                    <h2 className="streak-title">Streak</h2>
+                    <p className="streak-description">
+                        You have a streak of 5 days!
+                    </p>
+                </div>
+
+                <div className="quizzes">
+                    <img src={listIcon} alt="list icon" />
+                    <h2 className="quizzes-title">Platinum Quizzes</h2>
+                    <p className="quizzes-description">golang - intermediate</p>
+                    <p className="quizzes-description">JavaScript - beginner</p>
+                    <p className="quizzes-description">AWS - beginner</p>
+                </div>
+
+                <div className="account">
+                    <img src={accountIcon} alt="account icon" />
+                    <h2 className="account-title">lrnr Level: 2</h2>
+                    <p className="account-description">150/200 XP</p>
+                </div>
+            </div>
+        </main>
+    );
 }
