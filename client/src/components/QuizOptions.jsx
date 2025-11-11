5a6ff23c3124f1ff4b01d0ecb4450e72241ec1dd
N Manning <nmanning4@outlook.com>
2025-11-11 16:00:00 -0500

    Debugging header, edited Quiz styling

--- client/src/components/QuizOptions.jsx
+++ client/src/components/QuizOptions.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import '../styles/QuizOptions.css';
 import '../components/Button.jsx'
 
 export default function QuizOptions() {
