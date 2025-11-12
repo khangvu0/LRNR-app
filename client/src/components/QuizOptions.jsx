e1423725613b98a3b9366bf2fc7d9b6bd2f7ea11
N Manning <nmanning4@outlook.com>
2025-11-11 19:07:54 -0500

    re-added quizoptions.css, added necessary imports

--- client/src/components/QuizOptions.jsx
+++ client/src/components/QuizOptions.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import '../components/Button.jsx'
+import '../components/Button.jsx';
+import '../styles/QuizOptions.css';
 
 export default function QuizOptions() {
     // const { startQuiz, isLoading } = useQuiz(); 
